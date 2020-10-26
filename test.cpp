501df0e2b7de2f35ba8b0b32d0a9d2720c7885c6
Shu-Huai <lvzhihe123@qq.com>
2020-10-26 15:34:16 +0800

    add file

---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    virtual void foo()
+    {
+        cout << "A's foo()" << endl;
+        bar();
+    }
+    virtual void bar()
+    {
+        cout << "A's bar()" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void foo()
+    {
+        cout << "B's foo()" << endl;
+        A::foo();
+    }
+    void bar()
+    {
+        cout << "B's bar()" << endl;
+    }
+};
+int main()
+{
+    B bobj;
+    A *aptr = &bobj;
+    aptr->foo();
+    A aobj = *aptr; //转化为A类对象
+    aobj.foo();
+    return 0;
+}
