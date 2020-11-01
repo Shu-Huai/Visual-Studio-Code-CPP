17fd4f8d4db09fb6f3751dc45834d782a25fdac2
Shu-Huai <lvzhihe123@qq.com>
2020-11-01 23:52:10 +0800

    test for Liu Junyu

--- test.cpp
+++ test.cpp
@@ -1,37 +1,9 @@
-#include <iostream>
-using namespace std;
-class A
-{
-public:
-    virtual void foo()
-    {
-        cout << "A's foo()" << endl;
-        bar();
-    }
-    virtual void bar()
-    {
-        cout << "A's bar()" << endl;
-    }
-};
-class B : public A
-{
-public:
-    void foo()
-    {
-        cout << "B's foo()" << endl;
-        A::foo();
-    }
-    void bar()
-    {
-        cout << "B's bar()" << endl;
-    }
-};
+
+#include <stdio.h>
 int main()
 {
-    B bobj;
-    A *aptr = &bobj;
-    aptr->foo();
-    A aobj = *aptr; //转化为A类对象
-    aobj.foo();
+    float x = 10;
+    float y = 5;
+    printf("f(%.2f)=%.2f\n", x, y);
     return 0;
 }
