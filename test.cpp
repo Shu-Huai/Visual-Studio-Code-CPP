11a287a121ea8392341195e3c871dd3758e9d71c
Shu-Huai <lvzhihe123@qq.com>
2020-11-10 21:36:58 +0800

    modify for Liu Junyu

--- test.cpp
+++ test.cpp
@@ -1,9 +1,18 @@
-
 #include <stdio.h>
 int main()
 {
-    float x = 10;
-    float y = 5;
-    printf("f(%.2f)=%.2f\n", x, y);
+    int n, a, b, c, t = 0;
+    for (n = 1; n < 1000; n++)
+    {
+        c = n % 10;
+        b = (n / 10) % 100;
+        a = n / 100;
+        if ((n % 3 == 0) && ((a == 5) + (b == 5) + (c == 5)))
+            if (++t % 7)
+                printf("%6d", n);
+            else
+                printf("%6d\n", n);
+    }
+    printf("\n");
     return 0;
 }
