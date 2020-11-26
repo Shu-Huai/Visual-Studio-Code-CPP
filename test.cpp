97d15269ac4d1f4fd2620167ab4262fc75c598fa
Shu-Huai <lvzhihe123@qq.com>
2020-11-26 11:13:42 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,43 +1,20 @@
 #include <iostream>
 using namespace std;
-class Base1
+template <typename T>
+T test(T *n)
 {
-public:
-    Base1()
-    {
-        cout << "Base1 construct." << endl;
-    }
-};
-class Base2
-{
-public:
-    Base2(int x)
-    {
-        cout << "Base2 construct." << x << endl;
-    }
-};
-class Base3
-{
-public:
-    Base3(int y)
-    {
-        cout << "Base3 construct." << y << endl;
-    }
-};
-class Child : public Base2, public Base1, public Base3
-{
-public:
-    Child(int i, int j, int k, int m) : Base2(i), Base3(j), b2(k), b3(m)
-    {
-    }
-
-private:
-    Base1 b1;
-    Base2 b2;
-    Base3 b3;
-};
+    (*n)++;
+    return *n;
+}
 int main()
 {
-    Child child(3, 4, 5, 6);
+    int n = 114514;
+    test(&n);
+    double o = 114.514;
+    test(&o);
+    char c = '1';
+    test(&c);
+    char *d = &c;
+    test(&*d);
     return 0;
 }
