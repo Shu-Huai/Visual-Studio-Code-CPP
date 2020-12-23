2329bef6502cf4deb4d8e284c7941957755bd25f
Shu-Huai <lvzhihe123@qq.com>
2020-12-23 18:00:18 +0800

    added TestMultiSort()

--- Test Muti Threads.cpp
+++ Test Muti Threads.cpp
@@ -14,7 +14,7 @@ void InitData(ElemType *elems, int length)
     int i;
     for (i = 0; i < length; i++)
     {
-        elems[i] = (ElemType)UniformRand(54, 90);
+        elems[i] = (ElemType)UniformRand(32, 126);
     }
 }
 template <class ElemType>
@@ -26,7 +26,7 @@ ElemType &BubbleSort(ElemType *elems, int length)
         {
             if (elems[j] > elems[j + 1])
             {
-                int temp = elems[j];
+                ElemType temp = elems[j];
                 elems[j] = elems[j + 1];
                 elems[j + 1] = temp;
             }
@@ -47,79 +47,95 @@ bool Check(const ElemType *elems, int length)
     return true;
 }
 template <class ElemType>
-void TestSingleThread(int i, double &Time, bool &IsRight)
+void SingleThread(int i, double &Time, bool &IsRight)
 {
     clock_t StartTime;
     clock_t EndTime;
     ElemType *a = new ElemType[i];
     ElemType *b = new ElemType[i];
     ElemType *c = new ElemType[i];
+    ElemType *d = new ElemType[i];
     InitData(a, i);
     InitData(b, i);
     InitData(c, i);
+    InitData(d, i);
     StartTime = clock();
     BubbleSort(a, i);
     BubbleSort(b, i);
     BubbleSort(c, i);
+    BubbleSort(d, i);
     EndTime = clock();
-    Time = (double)(EndTime - StartTime) / CLOCKS_PER_SEC;
-    IsRight = Check(a, i) and Check(b, i) and Check(c, i);
+    Time = (float)(EndTime - StartTime) / CLOCKS_PER_SEC;
+    IsRight = Check(a, i) and Check(b, i) and Check(c, i) and Check(d, i);
     delete[] a;
     delete[] b;
+    delete[] c;
+    delete[] d;
 }
 template <class ElemType>
-void TestDoubleThreads(int i, double &Time, bool &IsRight)
+void DoubleThreads(int i, double &Time, bool &IsRight)
 {
     clock_t StartTime;
     clock_t EndTime;
     ElemType *a = new ElemType[i];
     ElemType *b = new ElemType[i];
     ElemType *c = new ElemType[i];
+    ElemType *d = new ElemType[i];
     InitData(a, i);
     InitData(b, i);
     InitData(c, i);
+    InitData(d, i);
     StartTime = clock();
     thread T0(BubbleSort<ElemType>, a, i);
     thread T1(BubbleSort<ElemType>, b, i);
     T0.join();
     T1.join();
-    BubbleSort<ElemType>(c, i);
+    T0 = thread(BubbleSort<ElemType>, c, i);
+    T1 = thread(BubbleSort<ElemType>, d, i);
+    T0.join();
+    T1.join();
     EndTime = clock();
-    Time = (double)(EndTime - StartTime) / CLOCKS_PER_SEC;
-    IsRight = Check(a, i) and Check(b, i) and Check(c, i);
+    Time = (float)(EndTime - StartTime) / CLOCKS_PER_SEC;
+    IsRight = Check(a, i) and Check(b, i) and Check(c, i) and Check(d, i);
     delete[] a;
     delete[] b;
     delete[] c;
+    delete[] d;
 }
 template <class ElemType>
-void TestTrippleThreads(int i, double &Time, bool &IsRight)
+void QuadrupleThreads(int i, double &Time, bool &IsRight)
 {
     clock_t StartTime;
     clock_t EndTime;
     ElemType *a = new ElemType[i];
     ElemType *b = new ElemType[i];
     ElemType *c = new ElemType[i];
+    ElemType *d = new ElemType[i];
     InitData(a, i);
     InitData(b, i);
     InitData(c, i);
+    InitData(d, i);
     StartTime = clock();
     thread T0(BubbleSort<ElemType>, a, i);
     thread T1(BubbleSort<ElemType>, b, i);
     thread T2(BubbleSort<ElemType>, c, i);
+    thread T3(BubbleSort<ElemType>, d, i);
     T0.join();
     T1.join();
     T2.join();
+    T3.join();
     EndTime = clock();
-    Time = (double)(EndTime - StartTime) / CLOCKS_PER_SEC;
-    IsRight = Check(a, i) and Check(b, i) and Check(c, i);
+    Time = (float)(EndTime - StartTime) / CLOCKS_PER_SEC;
+    IsRight = Check(a, i) and Check(b, i) and Check(c, i) and Check(d, i);
     delete[] a;
     delete[] b;
     delete[] c;
+    delete[] d;
 }
 template <class ElemType>
-void TestMutiThreads()
+void TestMultiThreads()
 {
-    for (int i = 1; i <= 65536; i *= 2)
+    for (int i = 1; i <= 131072; i *= 2)
     {
         double Time = 0;
         bool IsRight = 0;
@@ -129,19 +145,103 @@ void TestMutiThreads()
             if (!j)
             {
                 cout << "single";
-                TestSingleThread<ElemType>(i, Time, IsRight);
+                SingleThread<ElemType>(i, Time, IsRight);
             }
             else if (j == 1)
             {
                 cout << "double";
-                TestDoubleThreads<ElemType>(i, Time, IsRight);
+                DoubleThreads<ElemType>(i, Time, IsRight);
             }
             else
             {
-                cout << "tripple";
-                TestTrippleThreads<ElemType>(i, Time, IsRight);
+                cout << "quadruple";
+                QuadrupleThreads<ElemType>(i, Time, IsRight);
             }
-            cout << " thread to handle three of " << i << " " << abi::__cxa_demangle(typeid(ElemType).name(), 0, 0, 0) << " data is " << Time << "s." << endl
+            cout << " thread to handle four of " << i << " " << abi::__cxa_demangle(typeid(ElemType).name(), 0, 0, 0) << " data is " << Time << "s." << endl
+                 << "The result is " << (IsRight ? "true." : "false.") << endl;
+        }
+        cout << endl;
+    }
+}
+template <class ElemType>
+void SingleSort(int i, double &Time, bool &IsRight)
+{
+    clock_t StartTime;
+    clock_t EndTime;
+    ElemType *a = new ElemType[i];
+    InitData(a, i);
+    StartTime = clock();
+    BubbleSort<ElemType>(a, i);
+    EndTime = clock();
+    Time = (float)(EndTime - StartTime) / CLOCKS_PER_SEC;
+    IsRight = Check(a, i);
+    delete[] a;
+}
+template <class ElemType>
+void MultiSort(int i, double &Time, bool &IsRight)
+{
+    clock_t StartTime;
+    clock_t EndTime;
+    ElemType *a = new ElemType[i];
+    InitData(a, i);
+    ElemType *b = new ElemType[i];
+    ElemType *c = new ElemType[i];
+    StartTime = clock();
+    int bindex = 0;
+    int cindex = 0;
+    for (int j = 0; j < i; j++)
+    {
+        if (a[j] <= a[0])
+        {
+            b[bindex] = a[j];
+            bindex++;
+        }
+        else
+        {
+            c[cindex] = a[j];
+            cindex++;
+        }
+    }
+    thread T0(BubbleSort<ElemType>, b, bindex);
+    thread T1(BubbleSort<ElemType>, c, cindex);
+    T0.join();
+    T1.join();
+    for (int j = 0; j < bindex; j++)
+    {
+        a[j] = b[j];
+    }
+    for (int j = 0; j < cindex; j++)
+    {
+        a[j + bindex] = c[j];
+    }
+    EndTime = clock();
+    Time = (float)(EndTime - StartTime) / CLOCKS_PER_SEC;
+    IsRight = Check(a, i);
+    delete[] a;
+    delete[] b;
+    delete[] c;
+}
+template <class ElemType>
+void TestMultiSort()
+{
+    for (int i = 1; i <= 131072; i *= 2)
+    {
+        double Time = 0;
+        bool IsRight = 0;
+        for (int j = 0; j < 2; j++)
+        {
+            cout << "The runtime when using ";
+            if (!j)
+            {
+                cout << "single";
+                SingleSort<ElemType>(i, Time, IsRight);
+            }
+            else
+            {
+                cout << "double";
+                MultiSort<ElemType>(i, Time, IsRight);
+            }
+            cout << " thread to handle " << i << " " << abi::__cxa_demangle(typeid(ElemType).name(), 0, 0, 0) << " data is " << Time << "s." << endl
                  << "The result is " << (IsRight ? "true." : "false.") << endl;
         }
         cout << endl;
@@ -149,9 +249,22 @@ void TestMutiThreads()
 }
 int main()
 {
-    TestMutiThreads<short>();
-    TestMutiThreads<int>();
-    TestMutiThreads<float>();
-    TestMutiThreads<double>();
+    freopen("Test Muti Threads.txt", "w", stdout);
+    TestMultiThreads<short int>();
+    TestMultiThreads<int>();
+    TestMultiThreads<long int>();
+    TestMultiThreads<long long int>();
+    TestMultiThreads<float>();
+    TestMultiThreads<double>();
+    TestMultiThreads<long double>();
+    TestMultiThreads<char>();
+    TestMultiSort<short int>();
+    TestMultiSort<int>();
+    TestMultiSort<long int>();
+    TestMultiSort<long long int>();
+    TestMultiSort<float>();
+    TestMultiSort<double>();
+    TestMultiSort<long double>();
+    TestMultiSort<char>();
     return 0;
 }
