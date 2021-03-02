d5a000666c3e3b40de8a15c8202496ecd2cd77cb
殊怀丶 <lvzhihe_123@qq.com>
2021-03-02 16:56:20 +0800

    todo complete MinHeap.h

--- Data Struction/MinHeap.h
+++ Data Struction/MinHeap.h
@@ -11,7 +11,7 @@ private:
 
 public:
     MinHeap(int maxsize = DEFAULT_SIZE);
-    MinHeap(ElemType a[], int n, int maxsize = DEFAULT_SIZE);
+    MinHeap(ElemType e[], int n, int maxsize = DEFAULT_SIZE);
     ~MinHeap();
     Status Insert(const ElemType &e);
     Status DeleteTop(ElemType &e);
@@ -22,6 +22,51 @@ public:
     void SetEmpty();
     void Traverse(void (*Visit)(const ElemType &)) const;
 };
+template <class ElemType>
+void MinHeap<ElemType>::FilterDown(int start)
+{
+    int i = start, j;
+    ElemType temp = elems_[i];
+    j = 2 * i + l;
+    while (j <= size - 1)
+    {
+        if (j < size_ - 1 and elems_[j] > elems_[j + l])
+        {
+            j++;
+        }
+        if (temp <= elems_[j])
+        {
+            break;
+        }
+        else
+        {
+            elems_[i] = elmes_[j];
+            j = 2 * j + l;
+        }
+    }
+    elems_[i] ＝temp;
+}
+template <class ElemType>
+MinHeap<ElemType>::MinHeap(int maxsize) : size_(0), maxsize_(maxsize)
+{
+    elems_ = new ElemType[maxsize];
+    assert(elems_);
+}
+template <class ElemType>
+MinHeap<ElemType>::MinHeap(ElemType e[], int n, int maxsize) : size_(n), maxsize_(maxsize)
+{
+    elems_ = new ElemType[maxsize];
+    assert(elems_);
+    for (int i = 0; i < n; i++)
+    {
+        elems_[i] = e[i];
+    }
+    int i = (size_ - 2) / 2;
+    while (i >= 0)
+    {
+        FilterDown(i);
+    }
+}
 template <class ElemType>
 MinHeap<ElemType>::~MinHeap()
 {
