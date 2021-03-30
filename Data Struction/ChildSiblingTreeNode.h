6d8c60af0f9667e4d8a94bc0d9d42aad985d38df
殊怀丶 <lvzhihe_123@qq.com>
2021-03-30 21:06:19 +0800

    added ChildSiblingTreeNode.h

---
+++ Data Struction/ChildSiblingTreeNode.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef __CHILD_SIBLING_TREE_NODE_H__
+#define __CHILD_SIBLING_TREE_NODE_H__
+template <class ElemType>
+class ChildSiblingTreeNode
+{
+public:
+    ElemType data_;
+    ChildSiblingTreeNode<ElemType> *firstchild_;
+    ChildSiblingTreeNode<ElemType> *nextsibling_;
+    ChildSiblingTreeNode();
+    ChildSiblingTreeNode(ElemType data, ChildSiblingTreeNode<ElemType> *firstchild = NULL, ChildSiblingTreeNode<ElemType> *nextsibling = NULL);
+};
+template <class ElemType>
+ChildSiblingTreeNode<ElemType>::ChildSiblingTreeNode() : firstchild_(NULL), nextsibling_(NULL)
+{
+}
+template <class ElemType>
+ChildSiblingTreeNode<ElemType>::ChildSiblingTreeNode(ElemType data, ChildSiblingTreeNode<ElemType> *firstchild, ChildSiblingTreeNode<ElemType> *nextsibling)
+    : data_(data), firstchild_(firstchild), nextsibling_(nextsibling)
+{
+}
+#endif
