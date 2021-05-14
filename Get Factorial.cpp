54cc62b7ffbca691409fd67ce8eca63d74889e81
殊怀丶 <lvzhihe_123@qq.com>
2021-05-14 18:51:39 +0800

    added GetFactorialByLoop

--- Get Factorial.cpp
+++ Get Factorial.cpp
@@ -1,29 +1,60 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-long double GetFactorial(long double n)
+long double GetFactorialByLoop(int number)
 {
-    if (n < 0)
+    if (number < 0)
     {
         return 0;
     }
-    if (n == 0 || n == 1)
+    if (!number)
     {
         return 1;
     }
-    return n * GetFactorial(n - 1);
+    int i = 0;
+    long double result = 1;
+    for (i = 0; i < number; i++)
+    {
+        result *= (i + 1);
+    }
+    return result;
+}
+long double GetFactorialByRecursion(long double number)
+{
+    if (number < 0)
+    {
+        return 0;
+    }
+    if (!number)
+    {
+        return 1;
+    }
+    return number * GetFactorialByRecursion(number - 1);
 }
 int main()
 {
-    int i = 0;
+    int number = 0;
     cout << "Please input a number: ";
-    cin >> i;
-    long double result = GetFactorial(i);
-    for (int j = i; j > 0; j--)
+    cin >> number;
+    cout << number << "! by loop is: ";
+    for (int i = number; i > 0; i--)
     {
-        cout << j << " * ";
+        cout << i;
+        if (i != 1)
+        {
+            cout << " * ";
+        }
     }
-    cout << "\b\b\b"
-         << " = " << fixed << setprecision(0) << result << endl;
+    cout << " = " << fixed << setprecision(0) << GetFactorialByLoop(number) << endl;
+    cout << number << "! by recursion is: ";
+    for (int i = number; i > 0; i--)
+    {
+        cout << i;
+        if (i != 1)
+        {
+            cout << " * ";
+        }
+    }
+    cout << " = " << fixed << setprecision(0) << GetFactorialByRecursion(number) << endl;
     return 0;
 }
